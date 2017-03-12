43ada13966bf89c43dfb40b2379b21e4b70dfb6d
zekori96 <tobiasz.rumian@hotmail.com>
2017-03-12 14:27:43 +0100

    Lesson 2

--- src/main/java/com/currencyExchange/controller/CurrencyExchangeController.java
+++ src/main/java/com/currencyExchange/controller/CurrencyExchangeController.java
@@ -1,43 +1,57 @@
 package com.currencyExchange.controller;
 
+import com.currencyExchange.dto.CurrencyDTO;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.Currency;
 
 /**
  * Created by zekori on 07.03.17.
  */
-@RestController
+@RestController //With @RestController we don't need to add @ResponseBody to create a simple website.
 public class CurrencyExchangeController {
 
     @RequestMapping("/{number}")
-    public Long multiplyByTwo(@PathVariable Long number){
+    public Long multiplyByTwo(@PathVariable Long number) {
 
-        return number*2;
+        return number * 2;
     }
 
     @RequestMapping("/currency/{value}")
     public String addCurrencySignature(@PathVariable Long value,
-                                       @RequestParam("currency") String currency){
+                                       @RequestParam("currency") String currency) {
 
-        return value+currency;
+        return value + currency;
     }
-    @RequestMapping("/currencyValue/{value}/{multiplier}")//TODO: Add ability to type numbers with decimal point. {m:.+} is read as String
+
+    @RequestMapping("/currencyValue/{value}/{multiplier}")
+    //TODO: Add ability to type numbers with decimal point. {m:.+} is read as String
     public String valueOf(@PathVariable Long value,
                           @PathVariable Long multiplier,
                           @RequestParam("from") String from,
-                          @RequestParam("to") String to){
+                          @RequestParam("to") String to) {
 
-        try{
-            Currency currencyFrom= Currency.getInstance(from);
-            Currency currencyTo=Currency.getInstance(to);
-            return value+" "+currencyFrom.toString()+" = "+value*multiplier+" "+currencyTo.toString();
-        }catch (IllegalArgumentException e){
+        try {
+            Currency currencyFrom = Currency.getInstance(from);
+            Currency currencyTo = Currency.getInstance(to);
+            return value + " " + currencyFrom.toString() + " = " + value * multiplier + " " + currencyTo.toString();
+        } catch (IllegalArgumentException e) {
             e.getMessage();//if needed
             throw new RuntimeException("Something went wrong");
         }
     }
+
+    @RequestMapping("/dto")
+    public CurrencyDTO returnDTO(){
+        return CurrencyDTO.
+                builder().
+                currency(Currency.getInstance("PLN")).
+                value(new BigDecimal(123.32)).
+                build();
+    }
+
 }
--- src/main/java/com/currencyExchange/dto/CurrencyDTO.java
+++ src/main/java/com/currencyExchange/dto/CurrencyDTO.java
@@ -1,15 +1,17 @@
 package com.currencyExchange.dto;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 
 /**
  * Created by zekori on 09.03.17.
  */
 @lombok.Getter
 @lombok.Setter
+@lombok.Builder
 public class CurrencyDTO {
     private BigDecimal value;
-    private String currency;
+    private Currency currency;
 
 
 }
